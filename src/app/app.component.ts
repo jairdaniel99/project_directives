25d02100f327e65f7c8d950d2e948b809f52605d
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-07 14:27:21 -0600

    aphoto property binding & task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project_directives';
+  photo = {
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+    alt: 'Angular Logo',
+  };
 }
